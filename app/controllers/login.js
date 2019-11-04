ad1afef71779235a8d58c5a3e09278c0834e79de
Zubeir Mohamed <zumo@solute.de>
2019-11-04 02:00:12 +0100

    Finish authentication

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,20 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  session: service(),
+  toastr: service('toast'),
+
+  actions: {
+    login(){
+      if(this.channelname && this.password){
+        this.session.authenticate('authenticator:oauth2', this.channelname, this.password).catch((reason) => {
+          this.set('errorMessage', reason.error || reason);
+          console.log(reason.error || reason)
+        });
+      }else {
+        this.toastr.warning('Please enter all fields', 'Warning');
+      }
+    }
+  }
 });
